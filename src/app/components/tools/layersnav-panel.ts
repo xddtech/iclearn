b569106bfd32d53dc23eb53bd1dffefc83962861
Xuedong Din <xuedong.din@thomsonreuters.com>
2019-05-14 09:26:53 -0400

    before try dynamical component

---
+++ src/app/components/tools/layersnav-panel.ts
@@ -0,0 +1,26 @@
+import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+import {ElementDraggable} from '../../utils/element-draggable';
+import {AppService} from '../../services/app-service';
+import {AppStates} from '../../services/app-states';
+import {NeuronsModelView} from '../neurons/neurons-model-view';
+
+declare var $: any;
+
+@Component({
+    selector: 'layersnav-panel',
+    templateUrl: 'layersnav-panel.html',
+    styleUrls: ['./layersnav-panel.css'],
+    providers: [AppService, AppStates]
+ })
+ export default class LayersNavPanelComponent implements AfterViewInit {
+
+    constructor(private appService: AppService, private appStates: AppStates) {}
+ 
+    ngAfterViewInit() {
+       var top =  10 + this.appStates.getNavbarHeight();
+       $('#layersnav-panel').css('top', top + 'px');
+       $('#layersnav-panel').css('left', '10px');
+
+       ElementDraggable.register('layersnav-panel', {});
+    }
+ }
