a4d227ed9b3240d85acd908c2b021a8e8d0cd5c0
Xuedong Din <xuedong.din@thomsonreuters.com>
2019-06-29 09:52:18 -0400

    create new navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,7 +1,9 @@
 import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+import { Router } from '@angular/router';
 import {AppStates} from '../../services/app-states';
 import {AppConfig} from '../../app.config';
 
+declare var $: any;
 
 @Component({
    selector: 'app-navbar',
@@ -12,12 +14,41 @@ import {AppConfig} from '../../app.config';
 export default class NavbarComponent implements AfterViewInit {
    @ViewChild('appNavbar') navbarElement: ElementRef;
 
+   routeMap = {
+      'homeRoute': '/',
+      'modelRoute': '/model',
+      'aboutRoute': '/about',
+      'debugRoute': 'debug'
+   };
+   activeRoute = "";
    showDebugRoute = AppConfig.showDebugRoute;
 
-   constructor(private appStates: AppStates) {
+   constructor(private appStates: AppStates, private router: Router) {
    }
 
    ngAfterViewInit() {
       AppStates.navbarElem = this.navbarElement;
+   }
+
+   clickRoute($event: any) {
+      var routeId = $event.target.id;
+      this.routeTo(routeId);
+   }
+
+   routeTo(routeId: string) { 
+      if (routeId === this.activeRoute) {
+         return;
+      }
+      if (!this.routeMap[routeId]) {
+         console.error('route ' + routeId + ' is undefined in navbar');
+         return;
+      }
+      if (this.activeRoute) {
+         $('#' + this.activeRoute).removeClass('navitem-active');
+      }
+      this.activeRoute = routeId;
+      $('#' + this.activeRoute).addClass('navitem-active');
+
+      this.router.navigate([this.routeMap[this.activeRoute]]);
    }
 }
